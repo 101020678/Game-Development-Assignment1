6eca5d1b5a344d8db7b341fa35285449f7f18974
101020678 <navid.ghasemnejadnaybin@georgebrown.ca>
2017-10-15 16:18:52 -0400

    Rocket added

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+	[SerializeField]
+	float maxYspeed =5f;
+	[SerializeField]
+	float minYspeed =10f;
+	[SerializeField]
+	float maxXspeed =-2f;
+	[SerializeField]
+	float minXspeed =2f;
+
+	private Transform _trasnsform;
+	private Vector2 _currenSpeed;
+	private Vector2 _currenPosition;
+	// Use this for initialization
+	void Start () {
+		_trasnsform = gameObject.GetComponents<Transform> ();
+		Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Reset()
+	{
+		float xSpeed = Random.Range (minXspeed, maxXspeed);
+		float ySpeed = Random.Range (minYspeed, maxYspeed);
+
+		_currenSpeed = new Vector2 (xSpeed,ySpeed);
+	}
+}
