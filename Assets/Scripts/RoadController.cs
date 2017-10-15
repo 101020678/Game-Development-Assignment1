6c4e4a4d18018bed40cb7dce015e497fc0863f02
101020678 <navid.ghasemnejadnaybin@georgebrown.ca>
2017-10-15 03:46:52 -0400

    Moving Ocean applied

---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+	//Public variables 
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float startX;
+	[SerializeField]
+	private float endX;
+
+
+
+
+
+	//Private variables 
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPosition = _transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = _transform.position;
+		_currentPosition -= new Vector2 (speed, 0);
+		_transform.position = _currentPosition;
+	}
+}
